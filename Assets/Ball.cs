5f774abc3b188a9336fe165be268d5548c6938fb
concretemixer <max.pervukhin@gmail.com>
2016-04-26 10:18:39 +0600

    upd

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -3,18 +3,24 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
-    
+
 	// Use this for initialization
 	void Start () {
 
-        Vector3 v = new Vector3(0, -4.15f, 41.5f);
-        v.Normalize();
-        v *= 43;
 
-        GetComponent<Rigidbody>().velocity = v;
+        Vector3 v = new Vector3(-0.35f, 0.2f, 1);
         lifetime = 0;
+        v.Normalize();
+
+            v *= 28;
+            GetComponent<Rigidbody>().velocity = v;
+
         Debug.Log("v1 = " + GetComponent<Rigidbody>().velocity.magnitude);
+
+       
 	}
+
+    Vector3 f = Vector3.zero;
 
     float lifetime = 0;
 
@@ -32,31 +38,92 @@ public class Ball : MonoBehaviour {
 
         v.Normalize();
 
-        GetComponent<Rigidbody>().AddForce(new Vector3(0, -G, 0));
-        GetComponent<Rigidbody>().AddForce(v * (Air * vLen * vLen));
+        if (transform.position.y > 0)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, -G, 0));
+            GetComponent<Rigidbody>().AddForce(v * (Air * vLen * vLen));
+        }
 
-
-        if (GetComponent<Rigidbody>().velocity.y>0)
-            Debug.Log("posY = " + gameObject.transform.position.y);
+     
+        GetComponent<Rigidbody>().AddForce(f);
+        f = Vector3.zero;
     }    
 
 	// Update is called once per frame
+    bool keyB = false;
+    bool keyC = false;
+
 	void Update () {
 
         if (Input.GetKey(KeyCode.A))
         {
             GetComponent<Rigidbody>().isKinematic = false;
             float x = gameObject.transform.position.x;
-            gameObject.transform.position = new Vector3(x, 2.9f, -11.89f);
-            Time.timeScale = 0.5f;
+            gameObject.transform.position = new Vector3(4.09f, 1.3f, -11.89f);
+            Time.timeScale = 1f;
             Start();
         }
+        if (Input.GetKey(KeyCode.B))
+        {
+            if (!keyB)
+            {
+                float speed = GetComponent<Rigidbody>().velocity.magnitude;
+                Vector3 v;
+                if (transform.position.z>0)
+                    v = new Vector3(0, 0,-11.89f) - transform.position;
+                else
+                    v = new Vector3(0, 0, 11.89f) - transform.position;
+                v.Normalize();
+                v.y = 0.8f;
+                v.Normalize();
 
+                GetComponent<Rigidbody>().velocity = v * speed * 0.6f;
+
+                keyB = true;
+            }
+        }
+        else
+            keyB = false;
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            if (!keyC)
+            {
+                float speed = GetComponent<Rigidbody>().velocity.magnitude;
+                Vector3 v;
+                if (transform.position.z > 0)
+                    v = new Vector3(0, 1.3f, -11.89f) - transform.position;
+                else
+                    v = new Vector3(0, 0, 11.89f) - transform.position;
+                v.Normalize();
+                v.y = 0.6f;
+                v.Normalize();
+
+                GetComponent<Rigidbody>().velocity = v * speed * 1.5f;
+
+                keyC = true;
+            }
+        }
+        else
+            keyC = false;
        
 
 	}
 
-    void OnCollisionEnter(Collision collision) {
+    void OnTriggerEnter(Collider col) {
+
+        if (col.gameObject.tag == "Clay")
+        {
+            Vector3 v = GetComponent<Rigidbody>().velocity;
+            if (v.y < 0)
+            {
+                Debug.Log("v2 = " + GetComponent<Rigidbody>().velocity.magnitude);
+                v.y = -v.y * 0.75f;
+                v.x = v.x * 0.8f;
+                v.z = v.z * 0.8f;
+                GetComponent<Rigidbody>().velocity = v;
+            }
+        }
         return;
         Debug.Log("t = " + lifetime.ToString());
         Debug.Log("pos = " + gameObject.transform.position.z);
---
+++ Assets/Racket.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Racket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(-100, 0, 0));
+        if (Input.GetKey(KeyCode.RightArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 0));
+        if (Input.GetKey(KeyCode.UpArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 500));
+        if (Input.GetKey(KeyCode.DownArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -500));
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
